004fefb2e5b1e8880a139bcf7c460fbfa7ba774b
Daniel Couto Resende <160771990+Danilo1500@users.noreply.github.com>
2025-11-24 14:38:01 +0000

    Criação do projeto

---
+++ daniel_fatec/src/main/java/br/edu/fatecou/daniel_fatec/DanielFatecApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fatecou.daniel_fatec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanielFatecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanielFatecApplication.class, args);
+	}
+
+}
---
+++ daniel_fatec/src/test/java/br/edu/fatecou/daniel_fatec/DanielFatecApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatecou.daniel_fatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanielFatecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
